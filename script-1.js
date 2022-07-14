5259e89e5f1ee2338b9db4feadc4179e9833a916
Ana Prahova <anastasiaprahova@gmail.com>
2022-07-14 12:10:29 +0300

    JSON to JS and back

--- script-1.js
+++ script-1.js
@@ -1,1 +1,20 @@
-'first test js code'
+//first test js code
+console.log('Hello git');
+
+//converting Javascript into JSON
+const post = {
+    title: 'Movie',
+    likesQty: 46
+};
+
+JSON.stringify(post) //results in '{"title":"Movie","likesQty":46}'
+
+//converting JSON into Javascript
+const postStringified = JSON.stringify(post);
+
+JSON.parse(postStringified); 
+/* results in 
+{likesQty: 46,
+title: "Movie"} */
+
+
